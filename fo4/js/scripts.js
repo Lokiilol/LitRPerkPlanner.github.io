9b13e86469053ec29fe73de742ebee3a08b90a8b
Lokiilol <68520967+Lokiilol@users.noreply.github.com>
2024-04-07 13:37:39 +0100

    Update scripts.js

--- fo4/js/scripts.js
+++ fo4/js/scripts.js
@@ -230,10 +230,10 @@ $(function () {
     renderAll();
 
     $includeBobbleheads.on('click', function () {
-        let valShift = -2;
+        let valShift = -1;
         
         if (includeBobbleheads()) {
-            valShift = 2;
+            valShift = 1;
         }
         
         const $inputs = $(".list-special>li>span>input")
